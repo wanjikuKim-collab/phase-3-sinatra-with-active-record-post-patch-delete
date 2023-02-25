4373c84bf58a2f9e6b1fcfb2d2238d85452b4996
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-02-25 14:00:55 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,33 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  post '/reviews' do
+    # binding.pry
+    review = Review.create(
+    score: params[:score],
+    comment: params[:comment],
+    game_id: params[:game_id],
+    user_id: params[:user_id]
+    )
+
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+  
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
 end
